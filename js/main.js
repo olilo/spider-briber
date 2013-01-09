7571868e799bbc24b300fc6f56d3102714de386e
Oliver Lorenz <olorenz@cosmocode.de>
2013-01-09 17:40:00 +0100

    added fluff (shield) and animate map as test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+jQuery(function() {
+    function moveDown() {
+        jQuery(".map").animate({top: 500}, 5000, moveUp);
+    }
+    function moveUp() {
+        jQuery(".map").animate({top: 0}, 5000);
+    }
+    moveDown();
+});
