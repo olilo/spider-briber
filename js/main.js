11ac5ff535b4f6e248970d574efe81e33e2eb8f7
Oliver Lorenz <olorenz@cosmocode.de>
2013-01-14 20:04:44 +0100

    first attempt at keyboard input

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 // define starting variables
 
-
 var gates = {
     "gate1": {
         'open': false
         // TODO define position + width/height too for walk checking
     }
 };
+var player = {
+    health: 100,
+    points: 0,
+    increment: {
+        left: 0,
+        top: 0
+    },
+    target: {
+        left: 0,
+        top: 0
+    }
+};
+var keyboardIncrement = 10;
+
 
 /* walkable area is a list of rectangles that define the outline
  * where the player can move. The rectangles are given as:
@@ -33,18 +46,32 @@ function openGate(id) {
 
 function closeGate(id) {
     gates[id].open = false;
-    jQuery("#" + id).hide().removeClass("gate-open").addClass("gate-closed");
-    jQuery("#" + id).show('slow');
+    jQuery("#" + id).hide().removeClass("gate-open").addClass("gate-closed").show('slow');
+}
+
+function spiderAttacks() {
+    player.health -= 5;
+    updateHUD();
+}
+
+function updateHUD() {
+    jQuery(".health").html(player.health);
+    jQuery(".points").html(player.points);
 }
 
 jQuery(function() {
     // init variables
-    var player = jQuery(".player");
-    var playerWidth = player.width();
-    var playerHeight = player.height();
-    var playerOffsetX = - jQuery(".playingfield").offset().left - playerWidth / 2;
-    var playerOffsetY = - jQuery(".playingfield").offset().top - playerHeight / 2;
+    var $player = jQuery(".player");
+    var playingField = jQuery(".playingfield");
+    var playerWidth = $player.width();
+    var playerHeight = $player.height();
+    var playerOffsetX = - playingField.offset().left - playerWidth / 2;
+    var playerOffsetY = - playingField.offset().top - playerHeight / 2;
     var map = jQuery(".map");
+    player.target.left = $player.position().left;
+    player.target.top = $player.position().top;
+
+    updateHUD();
 
     // some debug functions to test level
     jQuery(".debug_animate_level").bind('click', function() {
@@ -60,14 +87,49 @@ jQuery(function() {
         return false;
     });
 
-    // player move/"attack" logic
-    jQuery(".playingfield").bind('click', function(e) {
-        // TODO check that player can walk where we clicked
+    playingField.bind('click', function(e) {
         var mapPosition = map.position();
-        var left = mapPosition.left + playerOffsetX + e.pageX;
-        var top = -mapPosition.top + playerOffsetY + e.pageY;
-        console.log(left + "--" + top);
+        player.target.left = mapPosition.left + playerOffsetX + e.pageX;
+        player.target.top = -mapPosition.top + playerOffsetY + e.pageY;
+        console.log(player.target.left + "--" + player.target.top);
+    });
 
+    jQuery("body").bind('keydown', function(e) {
+        switch(e.which) {
+            case 37:
+            case 65:
+                // left arrow or 'a'
+                player.increment.left = -keyboardIncrement;
+                break;
+            case 38:
+            case 87:
+                // up arrow or 'w'
+                player.increment.top = -keyboardIncrement;
+                break;
+            case 39:
+            case 68:
+                // right arrow or 'd'
+                player.increment.left = keyboardIncrement;
+                break;
+            case 40:
+            case 83:
+                // down arrow or 's'
+                player.increment.top = keyboardIncrement;
+                break;
+            default:
+                // no recognized keyboard shortcut
+                break;
+        }
+    }).bind("keyup", function(e) {
+        player.increment.left = 0;
+        player.increment.top = 0;
+    });
+
+    setTimeout(tick, 100);
+
+    // player move/"attack" logic
+    function movePlayer(left, top) {
+        // TODO also check that player can walk through an open door
         var isWalkable = false;
         for (var i = walkableArea.length - 1; i >= 0; i--) {
             var wx1 = walkableArea[i][0];
@@ -95,11 +157,28 @@ jQuery(function() {
         }
 
         if (isWalkable) {
-            player.stop().animate({left: left, top: top});
+            var distance = Math.sqrt(Math.pow($player.position().left - left, 2) + Math.pow($player.position().top - top, 2));
+            var duration = distance * 3;
+            $player.stop().animate({left: left, top: top}, {easing: 'linear', duration: duration});
+            player.target.left = left;
+            player.target.top = top;
         } else {
             // TODO show an animated red x at cursor instead
             //alert("Not walkable");
         }
-    });
+    }
+
+    function tick() {
+        var leftBase = $player.position().left;
+        var topBase = $player.position().top;
+
+        if (player.increment.left > 0 || player.increment.top > 0) {
+            movePlayer(leftBase + player.increment.left, topBase + player.increment.top);
+        } else if (player.target.left != leftBase || player.target.top != topBase) {
+            movePlayer(player.target.left, player.target.top);
+        }
+    }
+
+    setInterval(tick, 100);
 
 });
